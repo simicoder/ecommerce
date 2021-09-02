3814e8591786cef68e2f6406662af299148cdef7
szymon <szymon.popielarz@gmail.com>
2021-09-02 21:02:19 +0200

    checkout product view changed

--- components/atoms/CheckoutProduct/CheckoutProduct.tsx
+++ components/atoms/CheckoutProduct/CheckoutProduct.tsx
@@ -1,12 +1,11 @@
 import { memo } from 'react';
 import type { ProductType } from 'types';
 import Image from 'next/image';
-import Link from 'next/link';
+import Link from '@material-ui/core/Link';
 import Input from '@material-ui/core/Input';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import CloseIcon from '@material-ui/icons/Close';
 
 type CheckoutProductProps = {
@@ -18,44 +17,37 @@ type CheckoutProductProps = {
   readonly onRemoveItem: (product: ProductType & { quantity: number }) => void;
 };
 
-const useStyles = makeStyles((theme) => ({
-  input: {
-    width: theme.spacing(4),
-  },
-}));
-
 export const CheckoutProduct = memo<CheckoutProductProps>(
   ({ product, onChangeItemQuantity, onRemoveItem }) => {
-    const classes = useStyles();
-
     return (
       <Grid
         container
+        spacing={1}
         direction="row"
         alignItems="center"
         justifyContent="center"
         data-testid="checkout-product"
       >
-        <Grid xs={4} container item direction="row" alignItems="center" justifyContent="center">
-          <Image src={product.imgurl} alt={product.name} width="200" height="200" />
+        <Grid xs={3} container item direction="column" alignItems="center" justifyContent="center">
+          <Image src={product.imgurl} alt={product.name} width="100%" height="100%" />
         </Grid>
-        <Grid xs={2} container item direction="row" alignItems="center" justifyContent="center">
-          <Link href={`/products/${product.id}`}>
-            <Typography variant="h4">{product.name}</Typography>
+        <Grid xs={4} container item direction="column" alignItems="center" justifyContent="center">
+          <Link color="initial" href={`/products/${product.id}`}>
+            {product.name}
           </Link>
         </Grid>
-        <Grid xs={2} container item direction="row" alignItems="center" justifyContent="center">
+        <Grid xs={2} container item direction="column" alignItems="center" justifyContent="center">
           <Input
-            className={classes.input}
+            fullWidth
             value={product.quantity}
             type="number"
             onChange={(e) => onChangeItemQuantity(product, Number(e.target.value))}
           />
         </Grid>
-        <Grid xs={2} container item direction="row" alignItems="center" justifyContent="center">
+        <Grid xs={2} container item direction="column" alignItems="center" justifyContent="center">
           <Typography variant="h5">${product.quantity * product.price}</Typography>
         </Grid>
-        <Grid xs={2} container item direction="row" alignItems="center" justifyContent="center">
+        <Grid xs={1} container item direction="row" alignItems="center" justifyContent="center">
           <Button>
             <CloseIcon onClick={() => onRemoveItem(product)} />
           </Button>
